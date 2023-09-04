7e5e3a4981c576aef5cde4daa2029fbff9fd9097
Alexander Galea <agalea91@gmail.com>
2023-09-03 20:33:24 -0400

    Update

---
+++ README.md
@@ -0,0 +1,15 @@
+# Raspberry PI Fragment Frame
+
+Digital display of rotating book fragments from Notion.
+
+## Setup raspberry PI
+
+Follow guide:
+
+https://www.raspberrypi.com/tutorials/how-to-use-a-raspberry-pi-in-kiosk-mode/
+
+Copy `kiosk.sh` to home dir on raspberry pi.
+
+Copy `kiosk.service` from this repo to `/lib/systemd/system/kiosk.service`
+
+##
---
+++ kiosk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+xset s noblank
+xset s off
+xset -dpms
+
+unclutter -idle 0.5 -root &
+
+sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/alex/.config/chromium/Default/Preferences
+sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/alex/.config/chromium/Default/Preferences
+
+pushd /home/alex; screen -d -m python -m http.server
+
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://0.0.0.0:8000/ &
+
+while true; do
+   xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
+   sleep 10
+done
